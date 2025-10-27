41344a666613ec441bb8c6acab7b0b3c081889c8
Wahidur Rahman <wrrahman456@gmail.com>
2025-10-27 11:20:56 +0600

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,21 @@
+
+
+
+public class Exception {
+    public static void main(String[] args) {
+        try {
+            int x = 10;
+            int y = 0;
+            int result = x/y;
+            System.out.println("result "+result);
+        } catch (ArithmeticException e1) {
+            System.out.println("Exception handled" +e1);
+        }
+        catch (ArrayIndexOutOfBoundsException e2) {
+            System.out.println("Exception handled" +e2);
+        }
+        finally{
+            System.out.println("The Last Line");
+        }
+    }
+}
